2fe4c6e76ab3b60a70a5dbcffc962bc71916d7e5
MStuv <markstuver@MacBook-Pro.local>
2013-10-21 21:10:48 -0400

    Initial Commit

---
+++ UITableViewController Challenge Solution/MASAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MASAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Mark Stuver on 10/21/13.
+//  Copyright (c) 2013 Halo International Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MASAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
